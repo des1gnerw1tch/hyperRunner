e8289e50fe859c90950232810b19104924069b32
Zachary Walker-Liang <zachcanon@gmail.com>
2021-01-22 20:31:04 -0500

    Double jump start

--- hyperRunner/Assets/Scripts/PlayerMovement.cs
+++ hyperRunner/Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
   public float speed;
   public float jumpPower;
+
   private bool touchingGround;
+  private int jumpsLeft;
   [SerializeField] private Rigidbody2D rb;
   [SerializeField] private Animator animator;
 
   void Update()
   {
     transform.position = new Vector3 (transform.position.x + speed*Time.deltaTime, transform.position.y, transform.position.z);
-    if (Input.GetKeyDown("space") && touchingGround)
+    if (Input.GetKeyDown("space") && jumpsLeft > 0)
         {
             Jump();
         }
@@ -29,12 +31,14 @@ public class PlayerMovement : MonoBehaviour
 
   void Jump() {
     rb.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+    jumpsLeft-= 1;
     Debug.Log("Jumped");
   }
 
   void OnCollisionEnter2D(Collision2D other)  {
     if (other.gameObject.CompareTag("Ground")) {
       touchingGround = true;
+      jumpsLeft = 2;
       Debug.Log("Touched the ground");
       animator.SetBool("jumping", false);
     }
